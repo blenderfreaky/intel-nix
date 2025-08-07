b29677a430475edda8f7a1093f1037a193ffd3c4
blenderfreaky <blenderfreaky@mailbox.org>
2025-08-07 03:14:47 +0200

    package hdr histogram and vc intrinics

--- src/vc-intrinsics.nix
+++ src/vc-intrinsics.nix
@@ -1,5 +1,33 @@
-{stdenv}:
-stdenv.mkDerivation {
-  pname = "vc-intrinsics";
-  version = "todo";
-}
+{
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  cmake,
+  ninja,
+  python3,
+  llvmPackages_21,
+}: let
+  version = "60cea7590bd022d95f5cf336ee765033bd114d69";
+in
+  stdenv.mkDerivation {
+    pname = "vc-intrinsics";
+    inherit version;
+    #https://github.com/intel/vc-intrinsics
+    src = fetchFromGitHub {
+      owner = "intel";
+      repo = "vc-intrinsics";
+      rev = version;
+      hash = "sha256-1K16UEa6DHoP2ukSx58OXJdtDWyUyHkq5Gd2DUj1644=";
+    };
+
+    nativeBuildInputs = [
+      cmake
+      ninja
+      python3
+    ];
+    # buildInputs = [ llvmPackages_21.libllvm.dev ];
+
+    cmakeFlags = [
+      (lib.cmakeFeature "LLVM_DIR" "${lib.getDev llvmPackages_21.llvm}/lib/cmake/llvm")
+    ];
+  }
