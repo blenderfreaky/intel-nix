8d470f46fa5720bbf090d755722e5d188e259733
blenderfreaky <blenderfreaky@mailbox.org>
2025-08-14 04:22:28 +0200

    UMF update and cherry-pick sarnex' cmake fix onto latest release

--- src/unified-memory-framework.nix
+++ src/unified-memory-framework.nix
@@ -1,6 +1,7 @@
 {
   stdenv,
   fetchFromGitHub,
+  fetchpatch,
   lib,
   cmake,
   ninja,
@@ -13,11 +14,11 @@
   numactl,
   pkg-config,
   cudaPackages,
-  useJemalloc ? false,
+  useJemalloc ? true,
   cudaSupport ? false,
   levelZeroSupport ? true,
   ctestCheckHook,
-  buildTests ? false,
+  buildTests ? true,
   gtest,
   gbenchmark,
   python3,
@@ -25,7 +26,7 @@
   sphinx,
   buildDocs ? true,
 }: let
-  version = "1.0.0";
+  version = "1.0.1";
   tag = "v${version}";
   # This needs to be vendored, as they don't support using a pre-built version
   # and they compile with specific flags that the nixpkgs version doesn't (and shouldn't) set
@@ -54,13 +55,17 @@ in
 
     buildInputs =
       [
+        # Is this always needed?
         level-zero
         oneTBB
+        # TODO: Are these both needed?
+        # Also, should they be propagated?
         hwloc
         hwloc.dev
       ]
       ++ lib.optionals useJemalloc [
         jemalloc
+        # TODO: Are these needed?
         autogen
         autoconf
         automake
@@ -68,14 +73,14 @@ in
       ++ lib.optionals cudaSupport [
         cudaPackages.cuda_cudart
       ]
-      ++ lib.optionals buildTests [
+      ++ lib.optionals finalAttrs.doCheck [
         numactl
         gtest
         gbenchmark
       ];
 
     # TODO: Is this needed?
-    nativeCheckInputs = lib.optionals buildTests [
+    nativeCheckInputs = lib.optionals finalAttrs.doCheck [
       ctestCheckHook
     ];
 
@@ -83,22 +88,27 @@ in
       owner = "oneapi-src";
       repo = "unified-memory-framework";
       inherit tag;
-      sha256 = "sha256-nolnyxnupHDzz92/uFpIJsmEkcvD9MgI0oMX0V8aM1s=";
+      sha256 = "sha256-aS03kZDVh/TEjKialuV5/i7C65OddAKOda22ik7ZrPs=";
     };
 
-    postPatch =
-      ''
-        # The CMake tries to find out the version via git.
-        # Since we're not in a clone, git describe won't work.
-        substituteInPlace cmake/helpers.cmake \
-          --replace-fail "git describe --always" "echo ${tag}"
-      ''
-      + (lib.optionalString useJemalloc ''
-        # This fixes building with ninja
-        # See https://github.com/oneapi-src/unified-memory-framework/issues/1474
-        substituteInPlace CMakeLists.txt \
-          --replace-fail "\$(nproc)" "\$\$(nproc)"
-      '');
+    patches = [
+      (fetchpatch {
+        url = "https://github.com/oneapi-src/unified-memory-framework/commit/df9fcf13eefd804110d5cc4cbc661ec21febb7c7.patch";
+        hash = "sha256-g1Sir5S7zeIFqkQYMeLVpdbJjXvlWN69rEZu4qTwr9w=";
+      })
+    ];
+
+    postPatch = ''
+      # The CMake tries to find out the version via git.
+      # Since we're not in a clone, git describe won't work.
+      substituteInPlace cmake/helpers.cmake \
+        --replace-fail "git describe --always" "echo ${tag}"
+    '';
+
+    preConfigure = lib.optionalString useJemalloc ''
+      cp -r ${jemalloc} /build/jemalloc
+      chmod u+w /build/jemalloc
+    '';
 
     cmakeFlags = [
       (lib.cmakeBool "FETCHCONTENT_FULLY_DISCONNECTED" true)
@@ -109,21 +119,22 @@ in
 
       (lib.cmakeBool "UMF_BUILD_LIBUMF_POOL_JEMALLOC" useJemalloc)
 
-      (lib.cmakeBool "UMF_BUILD_TESTS" buildTests)
-      (lib.cmakeBool "UMF_BUILD_GPU_TESTS" buildTests)
-      (lib.cmakeBool "UMF_BUILD_BENCHMARKS" buildTests)
-      (lib.cmakeBool "UMF_BUILD_EXAMPLES" buildTests)
-      (lib.cmakeBool "UMF_BUILD_GPU_EXAMPLES" buildTests)
+      (lib.cmakeBool "UMF_BUILD_TESTS" finalAttrs.doCheck)
+      (lib.cmakeBool "UMF_BUILD_GPU_TESTS" finalAttrs.doCheck)
+      (lib.cmakeBool "UMF_BUILD_BENCHMARKS" finalAttrs.doCheck)
+      (lib.cmakeBool "UMF_BUILD_EXAMPLES" finalAttrs.doCheck)
+      (lib.cmakeBool "UMF_BUILD_GPU_EXAMPLES" finalAttrs.doCheck)
 
-      (lib.cmakeFeature "FETCHCONTENT_SOURCE_DIR_JEMALLOC_TARG" "${jemalloc}")
+      (lib.cmakeFeature "FETCHCONTENT_SOURCE_DIR_JEMALLOC_TARG" "/build/jemalloc")
+      (lib.cmakeFeature "FETCHCONTENT_SOURCE_DIR_GOOGLETEST" "${gtest.src}")
     ];
 
-    NIX_LDFLAGS = lib.optionalString buildTests "-rpath ${
+    doCheck = buildTests;
+
+    NIX_LDFLAGS = lib.optionalString finalAttrs.doCheck "-rpath ${
       lib.makeLibraryPath [
         oneTBB
         level-zero
       ]
     }";
-
-    doCheck = buildTests;
   })
