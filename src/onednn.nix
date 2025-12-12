1d26af790d312d8ee00302f61cc485db902f69ad
blenderfreaky <blenderfreaky@mailbox.org>
2025-12-12 20:40:40 +0100

    update onednn

--- src/onednn.nix
+++ src/onednn.nix
@@ -1,61 +1,84 @@
 {
-  llvm,
   cmake,
-  ninja,
   fetchFromGitHub,
-  tbb_2022,
-  opencl-headers,
-  ocl-icd,
-  gcc,
   lib,
+  stdenv,
+  llvmPackages,
+  intel-llvm,
+  gcc,
+  onetbb,
+  ocl-icd,
+  levelZeroSupport ? true,
+  rocmSupport ? true,
+  rocmPackages ? {},
 }: let
-  version = "3.9.1";
+  useSycl = levelZeroSupport || rocmSupport;
+  stdenv =
+    if useSycl
+    then intel-llvm.stdenv
+    else stdenv;
 in
-  llvm.stdenv.mkDerivation {
-    pname = "onednn";
-    inherit version;
+  # This was originally called mkl-dnn, then it was renamed to dnnl, and it has
+  # just recently been renamed again to oneDNN. See here for details:
+  # https://github.com/uxlfoundation/oneDNN#oneapi-deep-neural-network-library-onednn
+  stdenv.mkDerivation (finalAttrs: {
+    pname = "oneDNN";
+    version = "3.10.1";
 
     src = fetchFromGitHub {
       owner = "uxlfoundation";
       repo = "oneDNN";
-      tag = "v${version}";
-      hash = "sha256-DbLW22LgG8wrBNMsxoUGlacHLcfIBwqyiv+HOmFDtxc=";
+      rev = "v${finalAttrs.version}";
+      hash = "sha256-v1A9bOjcveTg97RBI2Y/gikoeQKYN8ZfFrqJmD3lVys=";
     };
 
-    nativeBuildInputs = [
-      cmake
-      ninja
+    outputs = [
+      "out"
+      "dev"
+      "doc"
     ];
 
-    buildInputs = [
-      tbb_2022
-      llvm.openmp
-      #llvm.merged
-      opencl-headers
-      ocl-icd
-      gcc
-    ];
+    nativeBuildInputs = [cmake] ++ lib.optionals useSycl [gcc];
 
-    # propagatedBuildInputs = [
-    #   opencl-headers
-    #   ocl-icd
-    # ];
+    buildInputs =
+      lib.optionals useSycl [
+        ocl-icd
+        onetbb
+      ]
+      ++ lib.optionals rocmSupport [
+        rocmPackages.clr # Provides HIP
+        rocmPackages.miopen
+        rocmPackages.rocblas
+      ]
+      ++ lib.optionals stdenv.hostPlatform.isDarwin [llvmPackages.openmp];
+
+    cmakeFlags =
+      [
+        (lib.cmakeFeature "ONEDNN_CPU_RUNTIME" "SYCL")
+        (lib.cmakeFeature "ONEDNN_GPU_RUNTIME" "SYCL")
+      ]
+      ++ lib.optionals rocmSupport [
+        (lib.cmakeFeature "ONEDNN_GPU_VENDOR" "AMD")
+        (lib.cmakeBool "ONEDNN_BUILD_GRAPH" false)
+      ];
+
+    # Tests fail on some Hydra builders, because they do not support SSE4.2.
+    doCheck = false;
 
     # Fixup bad cmake paths
     postInstall = ''
       substituteInPlace $out/lib/cmake/dnnl/dnnl-config.cmake \
-        --replace-fail "\''${PACKAGE_PREFIX_DIR}/" ""
+        --replace "\''${PACKAGE_PREFIX_DIR}/" ""
+
+      substituteInPlace $out/lib/cmake/dnnl/dnnl-targets.cmake \
+        --replace "\''${_IMPORT_PREFIX}/" ""
     '';
 
-    hardeningDisable = [
-      "zerocallusedregs"
-      "pacret"
-      # NOTE: Only produces warnings, so could be re-enabled
-      "shadowstack"
-    ];
-
-    cmakeFlags = [
-      (lib.cmakeFeature "ONEDNN_CPU_RUNTIME" "SYCL")
-      (lib.cmakeFeature "ONEDNN_GPU_RUNTIME" "SYCL")
-    ];
-  }
+    meta = {
+      changelog = "https://github.com/oneapi-src/oneDNN/releases/tag/v${finalAttrs.version}";
+      description = "oneAPI Deep Neural Network Library (oneDNN)";
+      homepage = "https://01.org/oneDNN";
+      license = lib.licenses.asl20;
+      platforms = lib.platforms.all;
+    };
+  })
